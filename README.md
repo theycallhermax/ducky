75a1d279a2e116295ac0b2756b6fbb95fb936292
Mwalters75 <pancake@mdwalters.ml>
2022-06-03 13:26:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ducky
+A GTK app for developers to make use of Rubber Duck Debugging without a physical duck.
+## What is this "Rubber Duck Debugging"?
+It's simple; you get a duck, walk the duck through your code, and you'll find the invalid code! Ducky makes it more simpler without disturbing your workflow, so you wouldn't have to look away from the computer everytime you need to debug!
---
+++ ducky.py
@@ -0,0 +1,18 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class ducky(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Ducky")
+        eventbox = Gtk.EventBox()        
+        self.add(eventbox)
+        image = Gtk.Image()
+        image.set_from_file("ducky.png")        
+        eventbox.add(image)
+
+window = ducky()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
